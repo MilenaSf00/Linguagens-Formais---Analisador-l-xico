353c5fe1eee695bcd885120ad584ce0af4e3eafb
MilenaSf00 <milenasoaresf2@gmail.com>
2023-06-29 19:23:33 -0300

    att

---
+++ Token.java
@@ -0,0 +1,17 @@
+public class Token {
+    private String tipo;
+    private String lexema;
+
+    public Token(String tipo, String lexema) {
+        this.tipo = tipo;
+        this.lexema = lexema;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getLexema() {
+        return lexema;
+    }
+}
